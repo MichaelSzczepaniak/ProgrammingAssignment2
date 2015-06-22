3fc95a80e4525171a53862f442d778eb15cfb7ca
MichaelSzczepaniak <mszczepaniak@pobox.com>
2015-06-22 16:37:08 -0600

    initial impl'n needs fixing and comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,39 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <- NULL
+    }
+    get <- function(x) {
+        x
+    }
+    setInverse <- function(invers) {
+        inv <<- invers
+    }
+    getInverse <- function() {
+        inv
+    }
+    
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    inv <- x$getInverse()
+    if(!is.null(inv)) {
+        # inverse has already been calculate
+        message("getting data from cache")
+    }
+    # inverse has been cached, so calculate it
+    dataMatrix <- x$get()  # get the data out of special "matrix" object (list)
+    inv <- solve(dataMatrix)
+    x$setInverse(inv)  # update the special "matrix" object (SMO) so that the
+                       # next time cacheSolve is called on the same SMO, the
+                       # cached value will be returned instead of recalculating
+    inv
 }
